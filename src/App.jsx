660f1f00040e39c101d353d7c1b3fbb845dd1e2f
webermuros1 <webermuros1@gmail.com>
2026-02-06 17:37:42 +0100

    fix:routes header from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from "./Pages/home/HomePage.jsx"           // ✅ .jsx + ruta correcta
 import AboutPage from "./Pages/aboutPage/AboutPage.jsx"        // ✅ about/ NO aboutPage/
-import Header from "./components/Header/Header.jsx"        // ✅ ruta correcta
-import Footer from "./components/Footer/Footer.jsx"        // ✅ ruta correcta
+import Header from "./components/header/Header.jsx"        // ✅ ruta correcta
+import Footer from "./components/footer/Footer.jsx"        // ✅ ruta correcta
 
 function App() {
   return (
